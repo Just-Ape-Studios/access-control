bd7c2e3c316a5cea88dece155e24ca02481d49d4
netfox <say-hi@netfox.rip>
2024-01-01 17:31:15 +0100

    convert u128 to a const slice of u8

--- internal.rs
+++ internal.rs
@@ -19,61 +19,67 @@ pub struct AccessControlData {
     ///
     ///   Emulating a nested map like the following using slices
     ///   should(???) work: mapping(account_id, mapping(idx, roles))
-    pub roles_per_account: Mapping<AccountId, u128>,
+    pub roles_per_account: Mapping<AccountId, [u8; 4]>,
 }
 
 #[repr(transparent)]
 #[derive(Debug, Copy, Clone, Default)]
-pub struct BitMap(pub u128);
+pub struct BitMap([u8; 4]);
 
 impl BitMap {
     #[inline]
-    /// set_bit changes `bit` to 1
-    pub fn set_bit(&mut self, bit: u8) -> &mut Self {
-        self.0 |= 1u128 << bit;
+    /// set_bit changes the bit at `pos` to 1
+    pub fn set_bit(&mut self, pos: usize) -> &mut Self {
+	let idx = pos / 8;
+	let off = pos % 8;
+        self.0[idx] |= 1u8 << off;
         self
     }
 
     #[inline]
-    /// clear_bit changes `bit` to 0
-    pub fn clear_bit(&mut self, bit: u8) -> &mut Self {
-        self.0 &= !(1u128 << bit);
+    /// clear_bit changes the bit at `pos` to 0
+    pub fn clear_bit(&mut self, pos: usize) -> &mut Self {
+	let idx = pos / 8;
+	let off = pos % 8;
+        self.0[idx] &= !(1u8 << off);
         self
     }
 
     #[inline]
-    /// has_bit_set returns true if `bit` is 1, false otherwise
-    pub fn has_bit_set(&self, bit: u8) -> bool {
-        (self.0 & (1 << bit)) > 0
+    /// has_bit_set returns true if the bit at `pos` is 1, false otherwise
+    pub fn has_bit_set(&self, pos: usize) -> bool {
+	let idx = pos / 8;
+	let off = pos % 8;
+        (self.0[idx] & (1 << off)) > 0
     }
 }
 
 impl AccessControlData {
-    pub fn set_role(&mut self, account_id: AccountId, role: u8) {
+    pub fn set_role(&mut self, account_id: AccountId, role: usize) {
         assert!(role <= 127, "can only define up to 128 roles");
 
         let account_roles = self
             .roles_per_account
             .get(account_id)
-            .map_or(*BitMap(0).set_bit(role), |roles| {
+            .map_or(*BitMap([0u8; 4]).set_bit(role), |roles| {
                 *BitMap(roles).set_bit(role)
             });
 
         self.roles_per_account.insert(account_id, &account_roles.0);
     }
 
-    pub fn unset_role(&mut self, account_id: AccountId, role: u8) {
+    pub fn unset_role(&mut self, account_id: AccountId, role: usize) {
         assert!(role <= 127, "can only define up to 128 roles");
 
         let account_roles = self
             .roles_per_account
             .get(account_id)
-            .map_or(BitMap(0), |roles| *BitMap(roles).clear_bit(role));
+            .map_or(BitMap([0u8; 4]), |roles| *BitMap(roles).clear_bit(role));
 
         self.roles_per_account.insert(account_id, &account_roles.0);
     }
 
-    pub fn has_role(&self, account_id: AccountId, role: u8) -> bool {
+    pub fn has_role(&self, account_id: AccountId, role: usize) -> bool {
         assert!(role <= 127, "can only define up to 128 roles");
 
         match self.roles_per_account.get(account_id) {
@@ -89,30 +95,36 @@ mod tests {
 
     #[test]
     fn test_bitmap_set_bit() {
-        let mut bm = BitMap(0);
+        let mut bm = BitMap([0u8; 4]);
 
-        bm.set_bit(0).set_bit(1).set_bit(127);
+        bm.set_bit(0).set_bit(1).set_bit(31);
 
-        assert_eq!(bm.0, (1 << 127) + 2 + 1);
+	assert_eq!(bm.0[0], 3);
+	assert_eq!(bm.0[1], 0);
+	assert_eq!(bm.0[2], 0);
+	assert_eq!(bm.0[3], 128);
     }
 
     #[test]
     fn test_bitmap_clear_bit() {
-        let mut bm = BitMap(u128::MAX);
+        let mut bm = BitMap([u8::MAX; 4]);
 
-        bm.clear_bit(0).clear_bit(1).clear_bit(127);
+        bm.clear_bit(0).clear_bit(1).clear_bit(31);
 
-        assert_eq!(bm.0, u128::MAX - (1 << 127) - 2 - 1);
+	assert_eq!(bm.0[0], u8::MAX - 2 - 1);
+	assert_eq!(bm.0[1], u8::MAX);
+	assert_eq!(bm.0[2], u8::MAX);
+	assert_eq!(bm.0[3], 127);
     }
 
     #[test]
     fn test_bitmap_has_bit_set() {
-        let bm = BitMap(0b1101u128);
+        let bm = BitMap([6, 2, 0, 0]);
 
-        assert_eq!(bm.has_bit_set(0), true);
-        assert_eq!(bm.has_bit_set(1), false);
+        assert_eq!(bm.has_bit_set(0), false);
+        assert_eq!(bm.has_bit_set(1), true);
         assert_eq!(bm.has_bit_set(2), true);
-        assert_eq!(bm.has_bit_set(3), true);
+        assert_eq!(bm.has_bit_set(9), true);
     }
 
     #[ink::test]
@@ -130,7 +142,7 @@ mod tests {
             .get(account)
             .unwrap_or_else(|| panic!());
 
-        assert_eq!(roles, 3);
+        assert_eq!(roles, [3, 0, 0, 0]);
     }
 
     #[ink::test]
@@ -156,7 +168,7 @@ mod tests {
             .get(account)
             .unwrap_or_else(|| panic!());
 
-        assert_eq!(roles, 5);
+        assert_eq!(roles, [5, 0, 0, 0]);
     }
 
     #[ink::test]
